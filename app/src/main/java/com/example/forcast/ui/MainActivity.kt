ee9cab9d5280da64cb05b5a6ef2b56dfb437ecdd
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-01-23 15:51:51 +0330

    migration from kotlin-exp to view binding

--- app/src/main/java/com/example/forcast/ui/MainActivity.kt
+++ app/src/main/java/com/example/forcast/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.forcast
+package com.example.forcast.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -6,18 +6,25 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.forcast.R
+import com.example.forcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        bottom_nav.setupWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
         NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
