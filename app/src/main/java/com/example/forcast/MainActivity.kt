2bf025497d002b435c3d462795abdc87d1fb478f
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-01-23 14:59:39 +0330

    menu and bottom navigation created

--- app/src/main/java/com/example/forcast/MainActivity.kt
+++ app/src/main/java/com/example/forcast/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.forcast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        bottom_nav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
